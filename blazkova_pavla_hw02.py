54106f0f7cfe2d67a54e3dbe799a56184c0c68c8
PavlaBla <paula.blazkova@gmail.com>
2024-06-12 13:44:30 +0200

    pridany popis kodu

--- blazkova_pavla_hw02.py
+++ blazkova_pavla_hw02.py
@@ -1,3 +1,8 @@
+# Zadani: Z prilozeneho tsv souboru vytvor pomoci JSON seznam slovniku,
+# kde u kazdeho filmu te budou zajimat jen urcite polozky.
+# Kazdy film prirad k dekade
+
+
 import json
 
 data = []
@@ -5,14 +10,21 @@ with open('netflix_titles.tsv', mode='r', encoding='utf-8') as file:
     for line in file:
         data.append(line.strip().split('\t'))
 
+# nacte soubor a po radku jej ulozi do seznamu data
+# vycisteni o prebytecne znaky, rozdeli radky pomoci '\t' delimiteru
+
 header = data[0]
 data = data[1:]
+
+# oddeli hlavicku od dat
 
 title_index = header.index('PRIMARYTITLE')
 director_index = header.index('DIRECTOR')
 cast_index = header.index('CAST')
 genre_index = header.index('GENRES')
 decade_index = header.index('STARTYEAR')
+
+# najde indexy sloupcu specifikovanych v zadani
 
 movies_info = []
 
@@ -26,6 +38,9 @@ for line in data:
     }
     movies_info.append(dict_movie)
 
+# vytvori seznam slovniku s klici podle zadani
 
 with open('hw02_output.json', mode='w', encoding='utf-8') as file:
     json.dump(movies_info, file, ensure_ascii=False, indent=4)
+
+# ulozi data do JSON souboru
